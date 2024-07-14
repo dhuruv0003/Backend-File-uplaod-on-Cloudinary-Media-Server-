670379dc549722232648730ff672252deb19e54a
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-07-14 13:44:20 +0530

    w commit

---
+++ config/cloudinary.js
@@ -0,0 +1,24 @@
+const cloudinary=require('cloudinary').v2
+
+require('dotenv').config();
+
+exports.cloudinaryConnect=()=>{
+    try {
+        //To establish connection of server with cloudinary we make use of config() funciton of cloudinary  
+        //Format=>  cloudinary.config({ 
+        //              cloud_name: --, 
+        //              api_key: --, 
+        //              api_secret: --
+        //           }); 
+
+        cloudinary.config({
+            cloud_name:process.env.CLOUD_NAME,
+            api_key:process.env.API_KEY,
+            api_secret:process.env.API_SECRET
+        })
+
+
+    } catch (error) {
+     console.error(error);   
+    }
+}
